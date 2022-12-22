078b3e43748880c3fd784ba44484b5d652502516
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2022-12-22 12:55:33 -0300

    Add files via upload

---
+++ assets/preload/data/overthink/Gameover.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bf pixel gm'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'pixelfnfloss'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'GameoverPixxel'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'GameoverpixelEnd'); --put in mods/music/
+end
